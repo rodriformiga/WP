07de554e30768269d570b4040c634d970a3d6a09
Rodrigo Pan <rodriformiga@gmail.com>
2016-03-15 15:23:45 -0300

    Initial Commit
    Initial Commit

---
+++ script_wp.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @id          Melhoria WP@@
+// @name        Melhoria WP@@
+// @namespace   Melhoria WP@@
+// @description Melhoria WP@@
+// @author      Rodrigo Pan
+// @copyright   2016, rodriformiga
+// @license     GNU GPLv3
+// @homepage    https://github.com/rodriformiga/WP/tree/master/Github_Comment_Enhancer#readme
+// @homepageURL https://github.com/rodriformiga/WP/tree/master/Github_Comment_Enhancer#readme
+// @downloadURL https://github.com/rodriformiga/WP/raw/master/Github_Comment_Enhancer/Github_Comment_Enhancer.user.js
+// @updateURL   https://github.com/rodriformiga/WP/raw/master/Github_Comment_Enhancer/Github_Comment_Enhancer.user.js
+// @supportURL  https://github.com/rodriformiga/WP/issues
+// @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VCYMHWQ7ZMBKW
+// @version     1.0.0
+// @icon        https://github.com/fluidicon.png
+// @run-at      document-end
+// @include        http://app3*:8081/wpp/servlet/hnuc010
+// @include        http://app3*:8081/wpp/servlet/hnuc011*
+// @include        http://app3*:8081/wpp/servlet/tsol080*
+// @include        http://app3*:8081/wpp/servlet/hwpp056*
+// @include        http://app3*:8081/wpp/servlet/hwpp111
+// @include        http://app3*:8081/wpp/servlet/hwpp112
+// @include        http://app3*:8081/wpp/servlet/hwpp210*
+// @include        http://app3*:8081/wpp/servlet/twpp025*
+// @include        http://app3*:8081/wpp/servlet/twpp045*
+// @include        http://app3*:8081/wpp/servlet/twpp093*
+// @include        http://app3*:8081/wpp/home/
+// @include        http://app3*:8081/wpp/servlet/hint001
+// @require        http://app3.focco.com.br:8081/wpp/sflselect.js
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @grant          GM_setValue
+// @grant          GM_getValue
+// ==/UserScript==
